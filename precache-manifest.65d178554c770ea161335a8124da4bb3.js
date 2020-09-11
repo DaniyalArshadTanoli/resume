7351002b5ed985c1996d14bb923598091246fa76
Daniyal Arshad <daniyalarshad059@gmail.com>
2020-09-12 00:23:46 +0500

    Updates

---
+++ precache-manifest.65d178554c770ea161335a8124da4bb3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6221688cd73cde8abf2f111a142ee2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cfcb0e465ddc5e76d30",
+    "url": "/static/css/2.b0e1eb24.chunk.css"
+  },
+  {
+    "revision": "3cfd16ea8cf3225c32f6",
+    "url": "/static/css/main.712808c7.chunk.css"
+  },
+  {
+    "revision": "5cfcb0e465ddc5e76d30",
+    "url": "/static/js/2.c560484a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c560484a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cfd16ea8cf3225c32f6",
+    "url": "/static/js/main.2829c216.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
